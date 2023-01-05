d16b8103d796265059be6d06528e1223cfc579ee
Jonathan Moraes <eng.jonathan.moraes@gmail.com>
2023-01-05 10:03:45 -0500

    broadcasting text messages

--- public/documento.js
+++ public/documento.js
@@ -3,5 +3,9 @@ const socket = io();
 const textoEditor = document.getElementById('editor-texto')
 
 textoEditor.addEventListener('keyup', () => {
-    console.log(textoEditor.value)
+    socket.emit('texto_editor', textoEditor.value)
+})
+
+socket.on('texto_editor_clientes', texto => {
+    textoEditor.value = texto
 })
--- src/socket-back.js
+++ src/socket-back.js
@@ -2,5 +2,9 @@ import io from './servidor.js';
 
 
 io.on('connection', (socket) => {
-    console.log(`Client connected with ID: ${socket.id}`)
+    console.log(`Client connected with ID: ${socket.id}`);
+
+    socket.on('texto_editor', (texto) => {
+        socket.broadcast.emit('texto_editor_clientes', texto)
+    })
 })
